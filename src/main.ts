b540166d957246feef57df76e54ff6ffe6c12f06
arundaya <runsworkspace@gmail.com>
2026-09-23 11:54:29 +0700

    feat: added global prefix

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(AppConfigService)
 
+  app.setGlobalPrefix('api/v1')
+
   app.useGlobalPipes(new ValidationPipe({
    whitelist: true,
    forbidNonWhitelisted: true,
