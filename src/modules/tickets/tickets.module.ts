ffe6670958b88f3e22458ba4c04fe979f69bc705
arundaya <runsworkspace@gmail.com>
2026-09-25 11:32:20 +0700

    feat: implement typeorm tickets repository and wire tickets module

--- src/modules/tickets/tickets.module.ts
+++ src/modules/tickets/tickets.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Ticket } from './entities/ticket.entity.js';
+import { TicketsRepository } from './domain/ports/tickets-repository.port.js';
+import { TypeOrmTicketsRepository } from './infra/typeorm-tickets.repository.js';
+import { TicketsService } from './tickets.service.js';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Ticket])],
+  providers: [
+    {
+      provide: TicketsRepository,
+      useClass: TypeOrmTicketsRepository,
+    },
+    TicketsService,
+  ],
+  exports: [TicketsService, TicketsRepository],
 })
 export class TicketsModule {}
