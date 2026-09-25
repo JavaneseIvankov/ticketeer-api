33fd6be89ea311063be502d5d25a308a9d446d44
arundaya <runsworkspace@gmail.com>
2026-09-25 11:58:45 +0700

    feat: implement tickets controller and register in tickets module

--- src/modules/tickets/tickets.module.ts
+++ src/modules/tickets/tickets.module.ts
@@ -4,9 +4,11 @@ import { Ticket } from './entities/ticket.entity.js';
 import { TicketsRepository } from './domain/ports/tickets-repository.port.js';
 import { TypeOrmTicketsRepository } from './infra/typeorm-tickets.repository.js';
 import { TicketsService } from './tickets.service.js';
+import { TicketsController } from './tickets.controller.js';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Ticket])],
+  controllers: [TicketsController],
   providers: [
     {
       provide: TicketsRepository,
